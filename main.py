44a461a497dac638a6fe3ca0a49fa14fad1b12a1
Naader <kooranaad@gmail.com>
2023-12-13 17:32:09 -0800

    cleaned up syntax, as well as created main function for user input

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from weather_api import get_current_weather
 
-api_key = 'fe52dd6395fb413f8d9ea3886042e8b9'
 
+# create main function
+def main():
+    print("Welcome to the Weather App!")
+
+    # get user input
+    city = input("Enter the city: ")
+    country = input("Enter the country code: ")
+    units = input("Enter units (e.g., 'M' for Metric, 'I' for Imperial): ")
+    language = input("Enter language code (e.g., 'en' for English): ")
+
+    # get api key
+    api_key = 'fe52dd6395fb413f8d9ea3886042e8b9'
+
+    # set all necessary parameters
+    weather_params = {
+        'api_key': api_key,
+        'city': city,
+        'country': country,
+        'units': units,
+        'language': language,
+    }
  params = {
         'key': api_key,
         'city': city,
@@ -10,8 +30,11 @@ api_key = 'fe52dd6395fb413f8d9ea3886042e8b9'
         'lang': language,
     }
 
-weather_data = get_weather(**weather_params)
+    weather_data = get_current_weather(**weather_params)
 
-if weather_data:
-    #process and use the weather data as needed
-    print(weather_data)
+    if weather_data:
+        # process and use the weather data
+        print(weather_data)
+    else:
+        print("Failed to retrieve weather data")
+    
