2619452bf883c19b60eced9aa44ecf18f6819711
Naader <kooranaad@gmail.com>
2023-12-14 01:53:09 -0800

    Created and perfected display_weather function. Separated metric and imperial units. Created intuitive user interface

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from weather_api import get_current_weather
 
 # create main function
 def main():
-    print("Welcome to the Weather App!")
+    print("\n\n==================================================")
+    print("\nWelcome to the Weather App!")
+    print("\n==================================================")
 
     # get user input
-    city = input("Enter the city: ")
+    city = input("\nEnter the city: ")
     country = input("Enter the country code: ")
     units = input("Enter units (e.g., 'M' for Metric, 'I' for Imperial): ")
     language = input("Enter language code (e.g., 'en' for English): ")
@@ -30,28 +32,55 @@ def main():
         'lang': language,
     }
 
+    
     weather_data = get_current_weather(**weather_params)
 
     if weather_data:
         # process and use the weather data
-        print(weather_data)
+        display_weather(weather_data, units)
     else:
         print("Failed to retrieve weather data")
 
+    
+
 
 # create display_weather function (display's weather for user)
-def display_weather(weather_data):
+def display_weather(weather_data, units):
+
     if 'data' in weather_data:
         current_data = weather_data['data'][0]
+        
+        temperature_unit = '°C'
+        if units == 'I':
+            temperature_unit = "°F"
 
-        print("Current Weather:")
-        print(f"Location: {current_data['city_name']}, {current_data['country_code']}")
-        print(f"Temperature: {current_data['temp']}°C")
+        wind_speed_unit = "m/s"
+        if units == 'I':
+            wind_speed_unit = "mph"
+
+        print("\n==================================================")
+        print("\nCurrent Weather:")
+        print("\n==================================================")
+        print(f"\nLocation: {current_data['city_name']}, {current_data['country_code']}")
+
+        temperature_value = current_data['temp']
+       
+        print("------------------------------------------------")
+        print(f"Temperature: {temperature_value}{temperature_unit}")
+        print("------------------------------------------------")
         print(f"Description: {current_data['weather']['description']}")
+        print("------------------------------------------------")
         print(f"Humidity: {current_data['rh']}%")
-        print(f"Wind Speed: {current_data['wind_spd']} m/s")
+
+        wind_speed_value = current_data['wind_spd']
+    
+        print("------------------------------------------------")
+        print(f"Wind Speed: {wind_speed_value} {wind_speed_unit}")
+        print("\n==================================================")
     else:
-        print("Invalid or incomplete weather data.")
+        print("\n==================================================")
+        print("\nInvalid or incomplete weather data.")
+        print("\n==================================================")
 
 
 if __name__ == "__main__":
